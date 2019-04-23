08610ddfc2c08b8c1f2d10d65f24e4a5d6ba76e5
John Bieling <john.bieling@gmx.de>
2019-04-23 16:35:20 +0200

    return directory instead of prefid

--- content/provider/eas/eas.js
+++ content/provider/eas/eas.js
@@ -408,14 +408,14 @@ var eas = {
      * return the id of the newAddressBook 
      */
     createAddressBook: function (newname, account, folderID) {
-        let abManager = Components.classes["@mozilla.org/abmanager;1"].getService(Components.interfaces.nsIAbManager);
+        let dirPrefId = MailServices.ab.newAddressBook(newname, "", 2);  /* kPABDirectory - return abManager.newAddressBook(name, "moz-abmdbdirectory://", 2); */
+        let directory = MailServices.ab.getDirectoryFromId(dirPrefId);
 
-        let dirPrefId = abManager.newAddressBook(newname, "", 2); /* kPABDirectory - return abManager.newAddressBook(name, "moz-abmdbdirectory://", 2); */
-        let data = abManager.getDirectoryFromId(dirPrefId);
-        if (data instanceof Components.interfaces.nsIAbDirectory && data.dirPrefId == dirPrefId) {
-            data.setStringValue("tbSyncIcon", "eas");
+        if (directory && directory instanceof Components.interfaces.nsIAbDirectory && directory.dirPrefId == dirPrefId) {
+            directory.setStringValue("tbSyncIcon", "eas");
+            return directory;		
         }
-        return dirPrefId; //change this to data on next big change
+        return null;
     },
 
 
