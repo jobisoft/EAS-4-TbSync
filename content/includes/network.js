jobisoft/EAS-4-TbSync

commit 94fbf4ad5f555d387a0951c466d75bd009eec887

Enable customizable client ID (#202)
* Make OAuth client ID configurable

This - possibly in combination with clientID.{type,useragent} - can help
in setups where some clients are permitted but not EAS-4-TbSync.

Signed-off-by: Jan Kiszka <jan.kiszka@web.de>

* Add a Makefile

For Unix devs.

Signed-off-by: Jan Kiszka <jan.kiszka@web.de>

---------

Signed-off-by: Jan Kiszka <jan.kiszka@web.de>

content/includes/network.js
@@ -78,6 +78,7 @@ var network = {
     let config = {};
     switch (host) {
       case "outlook.office365.com":
+        let customID = accountData.getAccountProperty("oauthClientID");
         config = {
           auth_uri : "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
           token_uri : "https://login.microsoftonline.com/common/oauth2/v2.0/token",
@@ -85,7 +86,7 @@ var network = {
           // changed in beta 1.14.1, according to
           // https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#default-and-consent
           scope : "offline_access https://outlook.office.com/.default", //"offline_access https://outlook.office.com/EAS.AccessAsUser.All",
-          client_id : "2980deeb-7460-4723-864a-f9b0f10cd992",
+          client_id : customID != "" ? customID : "2980deeb-7460-4723-864a-f9b0f10cd992",
         }
         break;
       

content/provider.js
@@ -40,6 +40,7 @@ var Base = class {
         branch.setIntPref("maxitems", 50);
         branch.setCharPref("clientID.type", "TbSync");
         branch.setCharPref("clientID.useragent", "Thunderbird ActiveSync");    
+        branch.setCharPref("oauth.clientID", "");
 
         eas.defaultTimezone = null;
         eas.utcTimezone = null;
@@ -265,6 +266,7 @@ var Base = class {
             "allowedEasCommands": "",
             "useragent": eas.prefs.getCharPref("clientID.useragent"),
             "devicetype": eas.prefs.getCharPref("clientID.type"),
+            "oauthClientID": eas.prefs.getCharPref("oauth.clientID"),
             "synclimit" : "7",
             }; 
         return row;

Makefile
@@ -1,0 +1,25 @@
+# This file is part of EAS-4-TbSync.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+PACKAGE_NAME=EAS-4-TbSync
+
+ARCHIVE_NAME=$(PACKAGE_NAME).xpi
+
+PACKAGE_FILES= \
+	content \
+	_locales \
+	manifest.json \
+	CONTRIBUTORS.md \
+	LICENSE README.md \
+	background.js
+
+all: clean $(PACKAGE_FILES)
+	zip -r $(ARCHIVE_NAME) $(PACKAGE_FILES)
+
+clean:
+	rm -f $(ARCHIVE_NAME)
+
+.PHONY: clean

commit b118a7c8ed136b3c0166d0acdee8884648446408

Fix O365 custom client ID setup
This was broken in 94fbf4ad: accountData is null during account setup,
causing the customID initialization and, thus, the complete account
creation to fail.

In fact, the client ID is not an account property, it's global.
Therefore introduce getCustomeOauthClientID to obtain this property
independent or the account.

Signed-off-by: Jan Kiszka <jan.kiszka@web.de>

content/includes/network.js
@@ -78,7 +78,7 @@ var network = {
     let config = {};
     switch (host) {
       case "outlook.office365.com":
-        let customID = accountData.getAccountProperty("oauthClientID");
+        let customID = eas.Base.getCustomeOauthClientID();
         config = {
           auth_uri : "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
           token_uri : "https://login.microsoftonline.com/common/oauth2/v2.0/token",

content/provider.js
@@ -266,7 +266,6 @@ var Base = class {
             "allowedEasCommands": "",
             "useragent": eas.prefs.getCharPref("clientID.useragent"),
             "devicetype": eas.prefs.getCharPref("clientID.type"),
-            "oauthClientID": eas.prefs.getCharPref("oauth.clientID"),
             "synclimit" : "7",
             }; 
         return row;
@@ -441,6 +440,14 @@ var Base = class {
 
         // Fall through, if there was no error.
         return new TbSync.StatusData();   
+    }
+
+
+    /**
+     * Return the custom OAuth2 ClientID.
+     */
+    static getCustomeOauthClientID() {
+        return eas.prefs.getCharPref("oauth.clientID");
     }
 }
 