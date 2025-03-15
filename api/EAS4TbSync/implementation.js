179f7bb00c63e047449fb80eab7e0a84732bab89
John Bieling <john.bieling@gmx.de>
2025-03-15 22:03:03 +0100

    fix scope issue of observeTbSyncInitialized listener

--- api/EAS4TbSync/implementation.js
+++ api/EAS4TbSync/implementation.js
@@ -9,32 +9,38 @@
 // Using a closure to not leak anything but the API to the outside world.
 (function (exports) {
 
-  var EAS4TbSync = class extends ExtensionCommon.ExtensionAPI {
-    async observeTbSyncInitialized (aSubject, aTopic, aData) {
-      let valid = false;
-      try {
-        var { TbSync } = ChromeUtils.importESModule("chrome://tbsync/content/tbsync.sys.mjs");
-        valid = TbSync.enabled;
-      } catch (e) {
-        // If this fails, TbSync is not loaded yet and we will get the notification later again.
-      }
+  const { ExtensionParent } = ChromeUtils.importESModule(
+    "resource://gre/modules/ExtensionParent.sys.mjs"
+  );
+  const easExtension = ExtensionParent.GlobalManager.getExtension(
+    "eas4tbsync@jobisoft.de"
+  );
 
-      //load this provider add-on into TbSync
-      if (valid) {
-        await TbSync.providers.loadProvider(this.extension, "eas", "chrome://eas4tbsync/content/provider.js");
-      }
+  async function observeTbSyncInitialized (aSubject, aTopic, aData) {
+    let valid = false;
+    try {
+      var { TbSync } = ChromeUtils.importESModule("chrome://tbsync/content/tbsync.sys.mjs");
+      valid = TbSync.enabled;
+    } catch (e) {
+      // If this fails, TbSync is not loaded yet and we will get the notification later again.
     }
 
-    getAPI(context) {
-      const that = this;
+    //load this provider add-on into TbSync
+    if (valid) {
+      await TbSync.providers.loadProvider(easExtension, "eas", "chrome://eas4tbsync/content/provider.js");
+    }
+  }
 
+  var EAS4TbSync = class extends ExtensionCommon.ExtensionAPI {
+
+    getAPI(context) {
       return {
         EAS4TbSync: {
           async load() {
-            Services.obs.addObserver(that.observeTbSyncInitialized, "tbsync.observer.initialized", false);
+            Services.obs.addObserver(observeTbSyncInitialized, "tbsync.observer.initialized", false);
 
             // Did we miss the observer?
-            that.observeTbSyncInitialized();
+            observeTbSyncInitialized();
           }
         },
       };
@@ -45,7 +51,7 @@
         return; // the application gets unloaded anyway
       }
 
-      Services.obs.removeObserver(this.observeTbSyncInitialized, "tbsync.observer.initialized");
+      Services.obs.removeObserver(observeTbSyncInitialized, "tbsync.observer.initialized");
       //unload this provider add-on from TbSync
       try {
         var { TbSync } = ChromeUtils.importESModule("chrome://tbsync/content/tbsync.sys.mjs");
